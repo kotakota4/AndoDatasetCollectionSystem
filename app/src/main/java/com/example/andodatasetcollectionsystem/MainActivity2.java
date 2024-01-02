1fb76c7f2d6326113c8fb96b2546770d07b55e3a
kotakota4 <kota4kota4@gmail.com>
2024-01-02 16:59:42 +0900

    ver2.0α

--- app/src/main/java/com/example/andodatasetcollectionsystem/MainActivity2.java
+++ app/src/main/java/com/example/andodatasetcollectionsystem/MainActivity2.java
@@ -5,8 +5,13 @@ import android.app.Activity;
 import android.bluetooth.BluetoothSocket;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -19,6 +24,7 @@ import android.provider.BaseColumns;
 import android.renderscript.ScriptGroup;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -50,7 +56,7 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity2 extends AppCompatActivity implements LocationListener {
+public class MainActivity2 extends AppCompatActivity implements LocationListener, SensorEventListener {
 
     BluetoothSocket socket;
     LocationManager locationManager;
@@ -65,11 +71,92 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
     private RPMCommand2 rpmCommand;
     private ThrottlePositionCommand2 throttlePositionCommand;
     private SQLiteDatabase db;
+    private CreateCsv createCsv;
+
+    int CREATE_DOCUMENT_REQUEST = 0;
+    int RESULT_OK = 1;
+    int RESULT_FAILED = 2;
 
     double latitude;
     double longitude;
 
+    ImageView rpmmeter;
+    ImageView pointer;
+
+    ImageView frictionCircle;
+    ImageView grippointer;
+
     Timer t;
+
+
+    float sensorX;
+    float sensorY;
+    float sensorZ;
+    float sensorX2;
+    float sensorY2;
+    float sensorZ2;
+    private SensorManager sensorManager;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+                Uri create_file = data.getData();  //取得した保存先のパスなど。
+
+                //出力処理を実行。その際の引数に上記のUri変数をセットする。
+                if (createCsv.exportCsv_for_SAF(create_file)) {
+                    //出力に成功した時の処理。
+                    Log.e("CreateCsv","Success to create csv");
+                } else {
+                    //出力に失敗した時の処理。
+                    Log.e("CreateCsv", "Fail to create csv");
+                }
+
+        //そもそもアクセスに失敗したなど、保存処理の前に失敗した時の処理。
+        Log.e("CreateCsv", "Fail to access to the db");
+
+        //リストの再読み込み
+        //this.LoadData();
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Sensor accel = sensorManager.getDefaultSensor(
+                Sensor.TYPE_ACCELEROMETER);
+
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Listenerを解除
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            sensorX = sensorX2;
+            sensorY = sensorY2;
+            sensorZ = sensorZ2;
+            sensorX2 = event.values[0];
+            sensorY2 = event.values[1];
+            sensorZ2 = event.values[2];
+            TranslateAnimation trance = new TranslateAnimation(sensorX*256, sensorX2*256, sensorZ*256, sensorZ2*256);
+            //TranslateAnimation trance = new TranslateAnimation(sensorX*100, sensorX2*100, sensorY*100, sensorY2*100);
+            trance.setFillAfter(true);
+            trance.setDuration(100);
+            grippointer.startAnimation(trance);
+
+            Log.i("acceleration",Float.valueOf(sensorX).toString()+" "+Float.valueOf(sensorY).toString());
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,7 +172,10 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
         ImageButton imageButton = findViewById(R.id.imageButton);
         TextView textView = findViewById(R.id.textView4);
         ImageButton imageButton1 = findViewById(R.id.imageButton2);
-
+        rpmmeter = findViewById(R.id.imageView1);
+        frictionCircle = findViewById(R.id.imageView2);
+        pointer = findViewById(R.id.imageView3);
+        grippointer = findViewById(R.id.imageView4);
 
         status = findViewById(R.id.textView2);
         rpm = findViewById(R.id.textView3);
@@ -95,8 +185,11 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
         Button button = findViewById(R.id.button2);
         Button buttonForSave = findViewById(R.id.button3);
         TextView textView1 = findViewById(R.id.textView5);
-
-
+        rpmmeter.setImageResource(R.drawable.rpm_meter);
+        frictionCircle.setImageResource(R.drawable.grip_circle);
+        pointer.setImageResource(R.drawable.pointer);
+        grippointer.setImageResource(R.drawable.grippointer);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         class ObdOnClickListener implements View.OnClickListener {
             public void onClick(View v) {
@@ -113,7 +206,7 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
                     socket = connect.getSocket();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
-                } catch (AdapterException.NotFoundException e1){
+                } catch (AdapterException.NotFoundException e1) {
                     textView.setText("OBDにアクセスできんかった");
                     Log.e("MainActivity2", "Error at OBD" + e1);
                     return;
@@ -121,7 +214,7 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
                     textView.setText("OBDが検出されなかった");
                     Log.e("MainActivity2", "Error at connection between OBD and smartphone" + e2);
                     return;
-                }catch (NullPointerException | IOException e3) {
+                } catch (NullPointerException | IOException e3) {
                     textView.setText("接続失敗, socketが空だった");
                     return;
                 }
@@ -129,21 +222,18 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
                 try {
                     status.setText("ステータス: connecting...");
                     InputStream inputStream = socket.getInputStream();
-                    OutputStream outputStream = socket. getOutputStream();
+                    OutputStream outputStream = socket.getOutputStream();
                     new EchoOffCommand().run(inputStream, outputStream);
                     Thread.sleep(1000);
                     new LineFeedOffCommand().run(inputStream, outputStream);
                     Thread.sleep(1000);
                     //new TimeoutCommand(125).run(inputStream, outputStream);
                     //Thread.sleep(1000);
-                    new SelectProtocolCommand(ObdProtocols.ISO_14230_4_KWP_FAST).run(inputStream, outputStream);
-
+                    new SelectProtocolCommand(ObdProtocols.AUTO).run(inputStream, outputStream);
                     Thread.sleep(1000);
-
-                    //Thread.sleep(1000);
-                    Log.i("send","success connecting obd");
+                    Log.i("send", "success connecting obd");
                     t = new Timer();
-                    t.scheduleAtFixedRate(new TimerTaskRPM(inputStream, outputStream), new Date(), 500);
+                    t.scheduleAtFixedRate(new TimerTaskRPM(inputStream, outputStream), new Date(), 1000);
                     status.setText("ステータス: collecting");
 
                 } catch (Exception e) {
@@ -157,21 +247,20 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
         class StopRPMOnClickListener implements View.OnClickListener {
 
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 status.setText("ステータス: stopped");
                 textView.setText("特になし");
-                if(t == null) return;
+                if (t == null) return;
                 t.cancel();
             }
         }
 
-        class ButtonForSaveListener implements View.OnClickListener{
+        class ButtonForSaveListener implements View.OnClickListener {
             @Override
-            public void onClick(View view){
-                Uri uri = Uri.parse("/data");
-                CreateCsv createCsv = new CreateCsv(MainActivity2.this, db);
+            public void onClick(View view) {
+                Uri uri = Uri.parse("/data/data/com.example.andodatasetcorrectionsystem/databases/");
+                createCsv = new CreateCsv(MainActivity2.this, db);
                 createCsv.fileOpen();
-                createCsv.exportCsv_for_SAF(uri);
             }
         }
 
@@ -217,7 +306,7 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
                             cursor.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_GPS_1));
                     String gps2 = cursor.getString(
                             cursor.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_GPS_2));
-                    text.append(milliToString(datetime) + ": " + rpm + " " + throttle + " " + gps1 + " " +  gps2 + "\n");
+                    text.append(milliToString(datetime) + ": " + rpm + " " + throttle + " " + gps1 + " " + gps2 + "\n");
                 }
                 cursor.close();
                 textView1.setText(text.toString());
@@ -261,20 +350,22 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
         locationText.setText("緯度 " + latitude + " 経度 " + longitude);
     }
 
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
     class TimerTaskRPM extends TimerTask{
         InputStream inputStream;
         OutputStream outputStream;
+        int numRPM;
+        float numThrottle;
 
         TimerTaskRPM(InputStream inputStream, OutputStream outputStream){
             super();
             this.inputStream = inputStream;
             this.outputStream = outputStream;
         }
-
-
-
-        int numRPM;
-        float numThrottle;
 
         @Override
         public void run() {
@@ -288,12 +379,15 @@ public class MainActivity2 extends AppCompatActivity implements LocationListener
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                
                 Log.i("time", "time is " + Long.valueOf(throttlePositionCommand.getEnd()-rpmCommand.getStart()).toString());
                 numRPM = rpmCommand.getRPM();
                 numThrottle = throttlePositionCommand.getPercentage();
                 Log.i("send","sending rpmcommand" + Integer.valueOf(numRPM).toString());
                 rpm.setText("回転数" + Integer.valueOf(numRPM).toString());
                 throttle.setText("スロットルポジション" + Float.valueOf(numThrottle).toString());
+                pointer.setRotation(-25+(int)numRPM*150/7000);
+
 
                 ContentValues values = new ContentValues();
                 values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TIME, System.currentTimeMillis());
